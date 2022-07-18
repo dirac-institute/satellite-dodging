9b3e947d1871b3d5a7e04dfa123da40cfa112400
Alina Hu <ahu@hmc.edu>
2022-07-18 11:25:09 -0700

    fixed function and added docstrings

--- find_streak_len.py
+++ find_streak_len.py
@@ -3,69 +3,109 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import healpy as hp
-
 from rubin_sim.data import get_data_dir
+
 import sqlite3
+import argparse
 
 from sat_utils import Constellation, starlink_constellation_v1
 
-def data_setup(): 
-
+def data_setup(datalocation): 
+    """
+    Loads up the dataframe given a datalocation 
+    Parameters
+    ----------
+    Param1 : str
+       the location of data 
+    
+    Returns
+    -------
+        dataframe 
+        dataframe that contains the data in that location
+    """
     limit = 900
 
-    dd = get_data_dir()
-    baseline_file = os.path.join(dd,'sim_baseline/baseline.db')
-
     # Conenct to the sqlite database
-    con = sqlite3.connect(baseline_file)
+    con = sqlite3.connect(datalocation)
 
     # Load up the first year
-    df = pd.read_sql('select * from observations where night < 1 and sunAlt > -24 ;', con)
+    df = pd.read_sql('select * from observations where night < 366;', con)
 
     con.close()
 
+    return df
 
-    # Make a satellite constellation
-    tles = starlink_constellation_v1()
-    constellation = Constellation(tles)
 
-    return df, constellation
+def compute_streak_len(datalocation, constellation,fieldRA='fieldRA',fieldDec='fieldDec',mjd='observationStartMJD', exptime='visitTime', id='observationId'): 
+    """
+    computes the streak length of satellite crossing 
+    Parameters
+    ----------
+    Param1 : datalocation
+       the location of data that contains the pointing informations.
+    Param2 : constellation
+       the constellation of satellites.
+    Param3 : fieldRA
+       the name of the dataframe column that gives information about the RA of the pointing.
+    Param4 : fieldDec
+       the name of the dataframe column that gives information about the Dec of the pointing.
+    Param5 : mjd
+       the name of the dataframe column that gives information about the observation mjd of the pointing.
+    Param6 : exptime
+       the name of the dataframe column that gives information about the exposure time of the pointing.
+    Param7 : id
+       the name of the dataframe column that gives information about the observation id of the pointing.
+    
+    Returns
+    -------
+        list
+        list with 3-Tuple elements containing observation id, the length of the streaks, and the number of streaks of each pointing.
+    """
+    if datalocation == None: 
+        dd = get_data_dir()
+        baseline_file = os.path.join(dd,'sim_baseline/baseline.db')
+        datalocation = baseline_file
 
-def first_method(df): 
-    ids=[]
-    lengths = []
-    nstreaks = []
-    n_rows = len(df)
-    t1 = time.time()
-    for index, row in df.iterrows():
-        length, streak = constellation.check_pointing(row['altitude'], row['azimuth'],
-                                        row['observationStartMJD'], row['visitTime'])
-        ids.append(df.loc[row,'observationId'])
-        lengths.append(length)
-        nstreaks.append(streak)
-        # A simple progress bar
-        progress = index/float(n_rows)*100
-        text = "\rprogress = %.3f%%" % progress
-        sys.stdout.write(text) 
-        sys.stdout.flush() 
-    t2 = time.time()
-    # print('runtime = %.2f min' % ((t2-t1)/60.) )
-    res=list(zip(ids,lengths,nstreaks))
+    df= data_setup(datalocation)
+    fast_lengths, fast_nstreaks = constellation.check_pointings(df[fieldRA].values, df[fieldDec].values,
+                                                           df[mjd].values,
+                                                           df[exptime].values)
+    ids=df.loc[:,id]
+
+    res=list(zip(ids,fast_lengths,fast_nstreaks))
 
     return res
 
 
-def second_method(df): 
-    t1 = time.time()
 
-    fast_lengths, fast_nstreaks = constellation.check_pointings(df['fieldRA'].values, df['fieldDec'].values,
-                                                           df['observationStartMJD'].values,
-                                                           df['visitTime'].values)
-    ids=df.loc[:,'observationId']
-    t2 = time.time()
-    # print('runtime = %.2f min' % ((t2-t1)/60.) )
 
-    res=list(zip(ids,fast_lengths,fast_nstreaks))
+if __name__ == '__main__':
 
-    return fast_lengths 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out_file", type=str, default='streak_len.npz')
+    parser.add_argument("--season_frac", type=float, default=0.1)
+    parser.add_argument("--datalocation", type=str, default = None)
+    parser.add_argument("--fieldRA", type=str, default = 'fieldRA')    
+    parser.add_argument("--fieldDec", type=str, default = 'fieldDec') 
+    parser.add_argument("--mjd", type=str, default = 'observationStartMJD') 
+    parser.add_argument("--exptime", type=str, default = 'visitTime') 
+    parser.add_argument("--id", type=str, default = 'observationId') 
+    args = parser.parse_args()
+    filename = args.out_file
+    season_frac = args.season_frac
+    datalocation = args.datalocation
+    tles = starlink_constellation_v1()
+    constellation = Constellation(tles)
+    fieldRA= args.fieldRA
+    fieldDec=args.fieldDec
+    mjd=args.mjd
+    exptime=args.exptime
+    id=args.id
+
+    obs_array = compute_streak_len(datalocation, constellation, fieldRA, fieldDec, mjd, exptime, id)
+    np.savez(filename, obs_array=obs_array)
+
+
+
+
 
